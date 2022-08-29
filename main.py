f4d2af11adadcce02467893c4b63b5cac6e86642
Nikita Glazkov <zitrnik@gmail.com>
2022-08-30 00:33:59 +0300

    DEV: save receipt text to db

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 from mark_time import mark_time
 from proxy_auth_data import proxy_ip, proxy_port, proxy_login, proxy_password
-from paths import receipts_path, db_path, data_path
+from paths import db_path
 from load_categories import get_categories
 
 headers = {
@@ -50,15 +50,27 @@ async def get_receipt_page_data(session, db_con, receipt_url):
             if recipe_steps_div := h2.findNext("div", class_="recipe-steps"):
                 receipt_text = recipe_steps_div.text.strip()
 
-        result = "".join(receipt_text.splitlines())
-        # TODO: добавить сохранение текста в БД и отметку об обработанности
-        tmp_path = data_path / "tmp.txt"
+        text_result = "".join(receipt_text.splitlines())
         try:
-            with open(tmp_path, "w", encoding="utf8") as tmp_file:
-                print("ВЫГРУЗИЛИ ТЕКСТ ДЛЯ РЕЦЕПТА ПО URL:", receipt_url)
-                json.dump(result, tmp_file, ensure_ascii=False, indent=4)
+            receipt_id_query = cursor.execute(
+                "SELECT id FROM receipts WHERE url=?", (receipt_url,)
+            )
+            receipt_id = receipt_id_query.fetchone()
+            receipt_id = receipt_id[0]
+            cursor.execute(
+                "INSERT OR IGNORE INTO receipts_data (receipt_id, text) VALUES (?, ?)",
+                (receipt_id, text_result),
+            )
+            cursor.execute("UPDATE receipts SET done=1 WHERE id=?", (receipt_id,))
+            db_con.commit()
         except Exception as e:
-            print(e)
+            print("e", e)
+            return None
+
+        await asyncio.sleep(
+            randrange(0, 1),
+        )
+        print(f"ВЫГРУЖЕН РЕЦЕПТ #{receipt_id} - {receipt_url}")
         return receipt_text
 
 
@@ -124,18 +136,38 @@ async def get_category_page_data(
 
         page += 1
         cursor.execute(
-            f"UPDATE categories SET next_page={page+1} WHERE id={category_id}"
+            "UPDATE categories SET next_page=? WHERE id=?",
+            (
+                page + 1,
+                category_id,
+            ),
         )
-    cursor.execute(f"UPDATE categories SET done=TRUE WHERE id={category_id}")
+    cursor.execute("UPDATE categories SET done=1 WHERE id=?", (category_id,))
     db_con.commit()
 
 
 async def gather_data(db_con):
     categories = get_categories(db_con)
+
+    cursor = db_con.cursor()
+    receipts_query = cursor.execute("SELECT url FROM receipts WHERE done=FALSE")
+    receipts = receipts_query.fetchall()
+
     # TODO: убрать ограничения
+    receipts = receipts[:3]
     categories = categories[:3]
     async with aiohttp.ClientSession() as session:
         tasks = []
+
+        # Добавление заданий для необработанных рецептов
+        for receipt in receipts:
+            receipt_url = receipt[0]
+            task = asyncio.create_task(
+                get_receipt_page_data(session, db_con, receipt_url)
+            )
+            tasks.append(task)
+
+        # Добавление заданий для категорий
         for category_id, category_url, next_page in categories:
             task = asyncio.create_task(
                 get_category_page_data(
@@ -149,15 +181,14 @@ async def gather_data(db_con):
 
 @mark_time
 def main():
-    # Создаём необходимые папки, если не существуют
-    if not receipts_path.exists() or not receipts_path.is_dir():
-        receipts_path.mkdir()
-
     # Создаём таблицы в БД, если не существуют
     db_con = sqlite3.connect(db_path)
     cur = db_con.cursor()
     cur.execute(
         "CREATE TABLE IF NOT EXISTS receipts (id INTEGER PRIMARY KEY AUTOINCREMENT, category_id INTEGER, title VARCHAR(255), url VARCHAR(255) UNIQUE, done BOOLEAN DEFAULT FALSE, FOREIGN KEY (category_id) REFERENCES categories (category_id) ON DELETE CASCADE)"
+    )
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS receipts_data (id INTEGER PRIMARY KEY AUTOINCREMENT, receipt_id INTEGER UNIQUE, text TEXT, FOREIGN KEY (receipt_id) REFERENCES receipts (receipt_id) ON DELETE CASCADE)"
     )
 
     # Запускаем асинхронные задачи
